96a3bb96cbfffd640d7533d623e3b5c16bc1001a
John Michael Gabalfin <21-00856@g.batstate-u.edu.ph>
2023-12-01 18:59:40 +0800

    CalendarPrototypeMade-Checkprog(27% done)
    Finally made a working prototype for a calendar using html css and javascript. Only need to reverse engineer for week calendar and other calendar designs.

--- src/app/pages/checkprog/checkprog.page.ts
+++ src/app/pages/checkprog/checkprog.page.ts
@@ -1,15 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checkprog',
   templateUrl: './checkprog.page.html',
   styleUrls: ['./checkprog.page.scss'],
 })
-export class CheckprogPage implements OnInit {
+export class CheckprogPage {
 
   constructor() { }
 
   ngOnInit() {
+    console.log("Hello")
+    // const daystag = document.querySelector(".days");
+
+    // let date = new Date(),
+    // currDay = date.getDay(),
+    // currMonth = date.getMonth(),
+    // currYear = date.getFullYear();
+
+    // var first = date.getDate() - currDay; //First day of month
+    // var last = first + 6; //Last day of month
+
+    // var firstday = new Date(date.setDate(first)).toUTCString();
+    // var lastday = new Date(date.setDate(last)).toUTCString();
+
+    // console.log(first, last, firstday, lastday);
+    // const renderCalendar = () => {
+    //   let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(); // gets last date of the month
+    //   let liTag = "";
+
+    //   for (let i = 1; i <= lastDateofMonth; i++) {
+    //     liTag += `<li>${i}</li>`; 
+    //   }
+
+    //   daystag.innerHTML = liTag;
+    // }
+
+    // renderCalendar();
+
+    const datesElement = document.getElementById('dates');
+
+    let currentDate = new Date();
+
+    const updateCalendar = () => {
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth();
+
+      const firstDay = new Date(currentYear, currentMonth, 0);
+      const lastDay = new Date(currentYear, currentMonth + 1, 0);
+      const totalDays = lastDay.getDate();
+      const firstDayIndex = firstDay.getDay();
+      const lastDayIndex = lastDay.getDay();
+
+      const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
+
+      let datesHTML = '';
+
+      for (let i = firstDayIndex; i > 0; i--){
+        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+        datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
+      }
+
+      for(let i = 1; i <= totalDays; i++){
+        const date = new Date(currentYear, currentMonth, i);
+        const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+        datesHTML += `<div class=date ${activeClass}">${i}</div>`;
+      }
+
+      for(let i = 1; i <= 7 - lastDayIndex; i++){
+        const nextDate = new Date(currentYear, currentMonth + 1, i);
+        datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
+      }
+
+      datesElement.innerHTML = datesHTML;
+    }
+
+    updateCalendar();
+
+    // const datesElement = document.getElementById('dates');
+
+    // let currentDate = new Date();
+
+    // const updateCalendar = () => {
+    //   const currentYear = currentDate.getFullYear();
+    //   const currentMonth = currentDate.getMonth();
+
+    //   const firstDay = new Date(currentYear, currentMonth, 0);
+    //   const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    //   const totalDays = lastDay.getDate();
+    //   const firstDayIndex = firstDay.getDay();
+    //   const lastDayIndex = lastDay.getDay();
+
+    //   const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
+    //   // monthYearElement.textContent = monthYearString;
+
+    //   let datesHTML = '';
+
+    //   for (let i = firstDayIndex; i > 0; i--){
+    //     const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+    //     datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
+    //   }
+
+    //   for(let i = 1; i <= totalDays; i++){
+    //     const date = new Date(currentYear, currentMonth, i);
+    //     const activeClass = date.toDateString() === new Date().toDateString() ? 'active' : '';
+    //     datesHTML += `<div class=date ${activeClass}">${i}</div>`;
+    //   }
+
+    //   for(let i = 1; i <= 7 - lastDayIndex; i++){
+    //     const nextDate = new Date(currentYear, currentMonth + 1, i);
+    //     datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
+    //   }
+
+    //   datesElement.innerHTML = datesHTML;
+    // }
+
+    // updateCalendar();
   }
 
 }
+
+
+
