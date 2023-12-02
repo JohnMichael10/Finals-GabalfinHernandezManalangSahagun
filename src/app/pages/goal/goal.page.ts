c785b56fa4175ae3ad764fc3f31eed4003b70558
PromiseSamManalang <126487199+PromiseSamManalang@users.noreply.github.com>
2023-12-03 03:06:20 +0800

    Goal update
    Added router to button and cleanup.

--- src/app/pages/goal/goal.page.ts
+++ src/app/pages/goal/goal.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { IonModal } from '@ionic/angular';
+import { Router } from '@angular/router';
 import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
@@ -10,50 +10,49 @@ import { OverlayEventDetail } from '@ionic/core/components';
 })
 export class GoalPage implements OnInit {
 
-  goalPage: boolean=false
-  bmiPage: boolean=false
-  mealPage: boolean=true
-  nutritionPage: boolean=false
-
+  goalPage: boolean = true;
+  bmiPage: boolean = true;
+  mealPage: boolean = true;
+  nutritionPage: boolean = true;
 
   selectedStep: string = 'step1'; // Default selected segment
 
-
-  bmiStats={
-    weight:0,
-    height:0,
-    bmi:0,
-    status:"",
-    target:0
-  }
-
-  mealPlan: { [key: string]: any } = {
-
+  bmiStats = {
+    weight: 0,
+    height: 0,
+    bmi: 0,
+    status: "",
+    target: 0
   };
 
-  isNormal:boolean=false
+  isNormal: boolean = false;
   @ViewChild(IonModal) modal: IonModal;
-  message="hello"
+  message = "hello";
 
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-  clickBmiPage(){
-    this.goalPage=false
-    this.bmiPage=true
-  }
-  clickMealPage(){
-    this.goalPage=false
-    this.mealPage=true
-  }
-  clickNutritionPage(){
-    this.goalPage=false
-    this.nutritionPage=true
+
+  goBack() {
+    // Use the router to navigate back
+    this.router.navigate(['/home']); // Adjust the route according to your application structure
   }
 
+  clickBmiPage() {
+    this.goalPage = false;
+    this.bmiPage = true;
+  }
+
+  clickMealPage() {
+    this.goalPage = false;
+    this.mealPage = true;
+  }
+
+  clickNutritionPage() {
+    this.goalPage = false;
+    this.nutritionPage = true;
+  }
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
@@ -61,9 +60,9 @@ export class GoalPage implements OnInit {
 
   confirm() {
     this.modal.dismiss(this.bmiStats.weight, 'confirm');
-    this.calculateBMI()
-    this.calculateStatus() //for displaying status
-    this.clickBmiPage()
+    this.calculateBMI();
+    this.calculateStatus(); //for displaying status
+    this.clickBmiPage();
   }
 
   calculateBMI() {
@@ -76,95 +75,28 @@ export class GoalPage implements OnInit {
       this.bmiStats.bmi = parseFloat(this.bmiStats.bmi.toFixed(2));
 
       this.bmiStats.target = 22 * Math.pow(heightInMeters, 2); // for target weight
-
     } else {
       // If weight or height is not provided
       this.bmiStats.bmi = null;
     }
   }
 
-
-  calculateStatus(){
-    if(this.bmiStats.bmi <=18.5){
-      this.bmiStats.status="Underweight"
+  calculateStatus() {
+    if (this.bmiStats.bmi <= 18.5) {
+      this.bmiStats.status = "Underweight";
+    } else if (this.bmiStats.bmi <= 24.9) {
+      this.bmiStats.status = "Normal";
+      this.isNormal = true;
+    } else if (this.bmiStats.bmi <= 30) {
+      this.bmiStats.status = "Overweight";
     }
-    else if(this.bmiStats.bmi <=24.9){
-      this.bmiStats.status="Normal"
-      this.isNormal=true
-    }
-    else if(this.bmiStats.bmi <=30){
-      this.bmiStats.status="Overweight"
-    }
-    if(this.bmiStats.bmi > 30){
-      this.bmiStats.status="Obese"
-    }
-
-
-  }
-
-
-
-  addMealInclusion(event: CustomEvent){
-    this.mealPlan={}
-
-    if(event.detail.value!=''){
-      event.detail.value.forEach((item:string) => {
-        this.mealPlan[item] = []
-      });
-      
+    if (this.bmiStats.bmi > 30) {
+      this.bmiStats.status = "Obese";
     }
   }
 
-  keyExists(key: string): boolean {
-    return key in this.mealPlan;
+  segmentChanged(event: CustomEvent) {
+    this.selectedStep = event.detail.value;
   }
-
-  addBreakfast(breakfast: CustomEvent){
-    this.mealPlan['breakfast']=[]
-    if(breakfast.detail.value!=''){
-      breakfast.detail.value.forEach((item:string) => {
-        this.mealPlan['breakfast'].push(item)
-      });
-    }
-  }
-
-  addLunch(lunch: CustomEvent){
-    this.mealPlan['lunch']=[]
-    if(lunch.detail.value!=''){
-      lunch.detail.value.forEach((item:string) => {
-        this.mealPlan['lunch'].push(item)
-      });
-    }
-  }
-
-  addDinner(dinner: CustomEvent){
-    this.mealPlan['dinner']=[]
-    if(dinner.detail.value!=''){
-      dinner.detail.value.forEach((item:string) => {
-        this.mealPlan['dinner'].push(item)
-      });
-    }
-  }
-
-  addSnack(snack: CustomEvent){
-    this.mealPlan['snack']=[]
-    if(snack.detail.value!=''){
-      snack.detail.value.forEach((item:string) => {
-        this.mealPlan['snack'].push(item)
-      });
-    }
-  }
-
-  mealPlanner(){
-    // deleting empty keys
-    Object.keys(this.mealPlan).forEach((key) => {
-      if (Array.isArray(this.mealPlan[key]) && this.mealPlan[key].length === 0) {
-          delete this.mealPlan[key];
-      }
-    });
-    
-    this.router.navigate(['/goal/meal-plan'], {
-      state: this.mealPlan
-    });
-  }
+  
 }
