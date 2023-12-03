096ea5fbd6aecd2e0d17ba3df368990f016d0a0d
mariusjacob-batstateu <21-07121@g.batstate-u.edu.ph>
2023-12-03 15:50:21 +0800

    bug fix

--- src/app/pages/goal/goal.page.ts
+++ src/app/pages/goal/goal.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonModal } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
@@ -10,61 +10,60 @@ import { OverlayEventDetail } from '@ionic/core/components';
 })
 export class GoalPage implements OnInit {
 
-  goalPage: boolean = true;
-  bmiPage: boolean = true;
-  mealPage: boolean = true;
-  nutritionPage: boolean = true;
+  // set a goal pages toggle
+  goalPage: boolean=true
+  bmiPage: boolean=false
+  mealPage: boolean=false
+  nutritionPage: boolean=false
 
-  selectedStep: string = 'step1'; // Default selected segment
 
-  bmiStats = {
-    weight: 0,
-    height: 0,
-    bmi: 0,
-    status: "",
-    target: 0
-  };
+  // BMI
+  bmiStats={
+    weight:0,
+    height:0,
+    bmi:0,
+    status:"",
+    target:0
+  }
+  isNormal:boolean=false 
+  // BMI
 
-  isNormal: boolean = false;
+
+  // Meal planner
+
+  mealPlan: { [key: string]: any } = {};
+
+
+  // Utilities
   @ViewChild(IonModal) modal: IonModal;
-  message = "hello";
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
 
-  goBack() {
-    // Use the router to navigate back
-    this.router.navigate(['/home']); // Adjust the route according to your application structure
-  }
 
-  clickBmiPage() {
-    this.goalPage = false;
-    this.bmiPage = true;
+  // Pages toggle
+  clickBmiPage(){
+    this.goalPage=false
+    this.bmiPage=true
   }
-
-  clickMealPage() {
-    this.goalPage = false;
-    this.mealPage = true;
+  clickMealPage(){
+    this.goalPage=false
+    this.mealPage=true
   }
-
-  clickNutritionPage() {
-    this.goalPage = false;
-    this.nutritionPage = true;
+  clickNutritionPage(){
+    this.goalPage=false
+    this.nutritionPage=true
   }
+  // Pages toggle
 
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-  }
 
-  confirm() {
-    this.modal.dismiss(this.bmiStats.weight, 'confirm');
-    this.calculateBMI();
-    this.calculateStatus(); //for displaying status
-    this.clickBmiPage();
-  }
 
+
+  // BMI
   calculateBMI() {
     if (this.bmiStats.weight && this.bmiStats.height) {
       // Convert height to meters
@@ -75,28 +74,112 @@ export class GoalPage implements OnInit {
       this.bmiStats.bmi = parseFloat(this.bmiStats.bmi.toFixed(2));
 
       this.bmiStats.target = 22 * Math.pow(heightInMeters, 2); // for target weight
+
     } else {
       // If weight or height is not provided
       this.bmiStats.bmi = null;
     }
   }
-
-  calculateStatus() {
-    if (this.bmiStats.bmi <= 18.5) {
-      this.bmiStats.status = "Underweight";
-    } else if (this.bmiStats.bmi <= 24.9) {
-      this.bmiStats.status = "Normal";
-      this.isNormal = true;
-    } else if (this.bmiStats.bmi <= 30) {
-      this.bmiStats.status = "Overweight";
+  calculateStatus(){
+    if(this.bmiStats.bmi <=18.5){
+      this.bmiStats.status="Underweight"
     }
-    if (this.bmiStats.bmi > 30) {
-      this.bmiStats.status = "Obese";
+    else if(this.bmiStats.bmi <=24.9){
+      this.bmiStats.status="Normal"
+      this.isNormal=true
+    }
+    else if(this.bmiStats.bmi <=30){
+      this.bmiStats.status="Overweight"
+    }
+    if(this.bmiStats.bmi > 30){
+      this.bmiStats.status="Obese"
+    }
+
+
+  }
+
+
+  // Meal planner
+  addMealInclusion(event: CustomEvent){
+    this.mealPlan={}
+
+    if(event.detail.value!=''){
+      event.detail.value.forEach((item:string) => {
+        this.mealPlan[item] = []
+      });
+      
     }
   }
 
-  segmentChanged(event: CustomEvent) {
-    this.selectedStep = event.detail.value;
+  keyExists(key: string): boolean {
+    return key in this.mealPlan;
   }
-  
+
+  addBreakfast(breakfast: CustomEvent){
+    this.mealPlan['breakfast']=[]
+    if(breakfast.detail.value!=''){
+      breakfast.detail.value.forEach((item:string) => {
+        this.mealPlan['breakfast'].push(item)
+      });
+    }
+  }
+
+  addLunch(lunch: CustomEvent){
+    this.mealPlan['lunch']=[]
+    if(lunch.detail.value!=''){
+      lunch.detail.value.forEach((item:string) => {
+        this.mealPlan['lunch'].push(item)
+      });
+    }
+  }
+
+  addDinner(dinner: CustomEvent){
+    this.mealPlan['dinner']=[]
+    if(dinner.detail.value!=''){
+      dinner.detail.value.forEach((item:string) => {
+        this.mealPlan['dinner'].push(item)
+      });
+    }
+  }
+
+  addSnack(snack: CustomEvent){
+    this.mealPlan['snack']=[]
+    if(snack.detail.value!=''){
+      snack.detail.value.forEach((item:string) => {
+        this.mealPlan['snack'].push(item)
+      });
+    }
+  }
+
+  mealPlanner(){
+    // deleting empty keys
+    Object.keys(this.mealPlan).forEach((key) => {
+      if (Array.isArray(this.mealPlan[key]) && this.mealPlan[key].length === 0) {
+          delete this.mealPlan[key];
+      }
+    });
+    
+    this.router.navigate(['/goal/meal-plan'], {
+      state: this.mealPlan
+    });
+  }
+  // MEAL PLANNER
+
+
+  // UTILITIES
+  goBack() {
+    // Use the router to navigate back
+    this.router.navigate(['/home']); // Adjust the route according to your application structure
+  }
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.bmiStats.weight, 'confirm');
+    this.calculateBMI()
+    this.calculateStatus() //for displaying status
+    this.clickBmiPage()
+  }
+  // UTILITIES
 }
